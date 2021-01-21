6324614bcd66149ffda4371383e9b4e956dd1c9c
taylor <taylorkanglin@gmail.com>
2021-01-21 10:08:07 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Button } from "element-ui";
+Vue.use(Button);
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="less"></style>

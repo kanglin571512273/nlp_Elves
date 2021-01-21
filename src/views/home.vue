65c78a05e3e2c848fca4681027c130e7f58a546c
taylor <taylorkanglin@gmail.com>
2021-01-21 17:50:49 +0800

    修改头部

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,7 +3,6 @@
     <el-container>
       <el-header>
         <div class="logo">
-          <img src="../assets/logo.png" alt />
         </div>
         <el-menu
           :default-active="activeIndex"
@@ -17,8 +16,9 @@
         >
           <el-menu-item route="/robot" index="1">机器人中心</el-menu-item>
           <el-menu-item route="/product" index="2">产品库</el-menu-item>
-          <el-menu-item route="/knowledge" index="3">知识库</el-menu-item>
-        </el-menu>.
+          <el-menu-item route="/knowledge" index="3" >知识库</el-menu-item
+          > </el-menu
+        >.
       </el-header>
       <el-main>
         <router-view />
@@ -40,14 +40,14 @@ export default {
   name: "home",
   data() {
     return {
-      activeIndex: "1",
+      activeIndex: "1"
     };
   },
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -60,10 +60,17 @@ export default {
   .el-header {
     background: #2d3291;
     box-sizing: border-box;
+    box-shadow: 0px 2px 7px 0px rgba(45, 50, 145, 0.5);
+
     display: flex;
     .logo {
+      width:100px;
+      height:100%;
+      background:url(../assets/images/logo.png) no-repeat;
+      background-size: cover;
       img {
-        width: 60px;
+        width: 100%;
+        height: 100%;
         margin: 0 10px;
       }
     }
